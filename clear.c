b0d8c9efa4330ded00c70065ab4c9589c0c1571b
Zeid Tisnes <ztisnes@e1z3r6p8.42.us.org>
2017-11-02 00:18:46 -0700

    few adds and fixes

--- clear.c
+++ clear.c
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   backtrack_knight.c                                 :+:      :+:    :+:   */
+/*   clear.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ztisnes <ztisnes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/23 09:51:13 by ztisnes           #+#    #+#             */
-/*   Updated: 2017/11/01 22:11:39 by ztisnes          ###   ########.fr       */
+/*   Created: 2017/11/01 23:48:50 by ztisnes           #+#    #+#             */
+/*   Updated: 2017/11/01 23:53:26 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "src/libft.h"
 #define SIZE 8
 
-void	print_board(void)
+void	clear_board(int SIZE, double **gameboard)
 {
-	char **gameboard;
 	int i;
 
 	i = -1;
-	gameboard = (char**)malloc(sizeof(char*) * SIZE);
-	while(++i < SIZE)
-	{
-		gameboard[i] = (char*)malloc(sizeof(char) * SIZE)
-		//Value of gameboard equal to '0'
-	}
+	while (++i < SIZE)
+		ft_memdel(gameboard[i])
+	ft_memdel(gameboard);
 }
-
-//int		backtrack_knight(int x, int y, )
-/*
-**	Gerenate 2D array
-**		if it cover all tours
-**			print the path
-*/
--- main.c
+++ main.c
@@ -6,11 +6,58 @@
 /*   By: ztisnes <ztisnes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/27 23:33:10 by ztisnes           #+#    #+#             */
-/*   Updated: 2017/10/28 00:36:47 by ztisnes          ###   ########.fr       */
+/*   Updated: 2017/11/02 00:17:04 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "src/libft.h"
+#include <stdio.h>
+#include <unistd.h>
+#define SIZE 8
+
+// char	**print_board(void)
+// {
+// 	char **gameboard; //char gameboard[SIZE][SIZE];
+// 	int i;
+// 	int j;
+//
+// 	i = -1;
+// 	gameboard = (char**)malloc(sizeof(char*) * SIZE);
+// 	while(++i < SIZE)
+// 	{
+// 		gameboard[i] = (char*)malloc(sizeof(char*) * SIZE);
+// 		j = -1;
+// 		while (++j < SIZE)
+// 		{
+// 			//gameboard[i][j] = (char*)malloc(sizeof(char*) * SIZE);
+// 			gameboard[i][j] = '0';
+// 			printf("%c", gameboard[i][j]);
+// 		}
+// 		//Value of gameboard equal to '0'
+// 		printf("\n");
+// 	}
+// 	return &(*gameboard);
+// }
+
+void	**print_board(void)
+{
+	char **gameboard;
+	int i;
+
+	i = -1;
+	gameboard = (char**)malloc(sizeof(char*) * SIZE);
+	if (gameboard == NULL)
+		return (NULL);
+	while(++i < SIZE)
+	{
+		gameboard[i] = (char*)malloc(sizeof(char) * SIZE);
+		*gameboard[i] = '0';
+		printf("%s", gameboard[i]);
+		//Value of gameboard equal to '0'
+	}
+	return *(*gameboard);
+}
+
 int		main()
 {
 	print_board();
